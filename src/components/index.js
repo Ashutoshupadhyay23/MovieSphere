913d8b30de3105a36c1ed6dd80d175158a34a5b3
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-07-16 12:56:42 +0530

    implementing search bar

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import Movies from "./Movies";
-import Watchlist from "./Watchlist";
-import Banner from "./Banner";
-import MovieCard from "./MovieCard";
+import Movies from "./movies/Movies";
+import Watchlist from "./watchlist/Watchlist";
+import Banner from "./movies/Banner";
+import MovieCard from "./movies/MovieCard";
 
 export {Movies, Watchlist, Banner, MovieCard}
---
+++ src/components/watchlist/Watchlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Watchlist = () => {
+  return (
+    <>
+      <div className='flex justify-center my-4'>
+        <input type="text" placeholder='Search movies' className='h-[3rem] w-[18rem] bg-gray-300 outline-none px-4 text-base text-black rounded-lg' />
+      </div>
+    </>
+    
+  )
+}
+
+export default Watchlist
