59b59d4eefd25076ebba997dbc7b5bddb56ff892
Ashutosh Upadhyay <upadhyayashutosh423@gmail.com>
2024-07-16 14:54:31 +0530

    worked on watchlist UI and fixed some edge cases on pagination

--- src/components/movies/Pagination.jsx
+++ src/components/movies/Pagination.jsx
@@ -3,23 +3,39 @@ import React from 'react';
 const Pagination = ({ pageNo, totalPages, handlePrev, handleNext, loading }) => {
   return (
     <div className="flex justify-center mt-5 w-full">
-      <button
-        className={`bg-gray-200 text-gray-700 py-2 px-4 rounded-l w-full ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-        onClick={handlePrev}
-        disabled={pageNo === 1 || loading}
-      >
-        {loading ? 'Loading...' : <i className="fa-solid fa-arrow-left"></i>}
-      </button>
-      <span className="bg-gray-200 text-gray-700 py-2 px-4 w-full text-center">
-        Page {pageNo}
-      </span>
-      <button
-        className={`bg-gray-200 text-gray-700 py-2 px-4 rounded-r w-full ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-        onClick={handleNext}
-        disabled={pageNo === totalPages || loading}
-      >
-        {loading ? 'Loading...' : <i className="fa-solid fa-arrow-right"></i>}
-      </button>
+      {loading ? (
+        <div className="flex items-center">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900 mr-2"></div>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <>
+          <button
+            className={`bg-gray-200 text-gray-700 py-2 px-4 rounded-l w-full ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={handlePrev}
+            disabled={pageNo === 1}
+          >
+            <div className='text-blue-500'>
+              <i className="fa-solid fa-arrow-left text-blue-500 mr-1"></i>
+              Back
+            </div>
+          </button>
+          <span className="bg-gray-200 text-gray-700 py-2 px-4 w-full text-center">
+            Page {pageNo}
+          </span>
+          <button
+            className={`bg-gray-200 text-gray-700 py-2 px-4 rounded-r w-full ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={handleNext}
+            disabled={pageNo === totalPages}
+          >
+            <div className='text-blue-500'>
+              Next
+              <i className="fa-solid fa-arrow-right text-blue-500 ml-1"></i>
+            </div>
+          </button>
+        </>
+      )}
+     
     </div>
   );
 };
--- src/components/watchlist/Watchlist.jsx
+++ src/components/watchlist/Watchlist.jsx
@@ -3,9 +3,53 @@ import React from 'react'
 const Watchlist = () => {
   return (
     <>
-      <div className='flex justify-center my-4'>
+
+      <div className='flex justify-center flex-wrap m-4'>
+        <div className='flex justify-center h-[2.5rem] w-[6rem] bg-blue-400 rounded-xl text-white font-medium items-center mx-2'>
+          Action
+        </div>
+        <div className='flex justify-center h-[2.5rem] w-[6rem] bg-gray-400/50 rounded-xl text-white font-medium items-center mx-2'>
+          Action
+        </div>
+      </div>
+
+      <div className='flex justify-center my-8'>
         <input type="text" placeholder='Search movies' className='h-[3rem] w-[18rem] bg-gray-300 outline-none px-4 text-base text-black rounded-lg' />
       </div>
+
+      <div className='border border-gray-100 m-6 overflow-hidden rounded-lg'>
+        <table className='w-full text-gray-500 text-center'>
+          <thead className='border-b-2'>
+            <tr className='h-9'>
+              <th>Name</th>
+              <th>Ratings</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr className='border-b-2'>
+              <td className='flex items-center px-4 py-2'>
+                <img className='h-[5rem] w-[8rem]' src={`https://images.pexels.com/photos/26793685/pexels-photo-26793685/free-photo-of-a-dog-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load`} />
+
+                <div className='mx-10'>
+                  The Matrix
+                </div>
+              </td>
+
+              <td>8</td>
+              <td>9</td>
+              <td>Action</td>
+
+              <td className='text-red-700'>Delete</td>
+
+            </tr>
+          </tbody>
+
+        </table>
+      </div>
+
     </>
     
   )
